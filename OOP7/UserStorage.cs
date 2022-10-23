9aa928a19fb3f986dc9f93d78c1a7c57d9e1f808
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-23 18:41:37 +0300

    Добавьте файлы проекта.

---
+++ OOP7/UserStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7
+{
+    internal class UserStorage
+    {
+        private Dictionary<AuthorizeInfo, UserInfo> data;
+
+        public UserStorage()
+        {
+            data= new Dictionary<AuthorizeInfo, UserInfo>();
+            data.Add(new AuthorizeInfo("Геворгян", "123"), new UserInfo("Геворгян Э.Г.", "gevorgyan@mail.ru","Stavropol"));
+            data.Add(new AuthorizeInfo("Адам", "321"), new UserInfo("Зайгов А.М.", "Zaigov@mail.ru", "Stavropol"));
+        }
+
+        public UserInfo GetUser(AuthorizeInfo ob)
+        {
+            if(data.ContainsKey(ob)) return data[ob];
+            else return null;
+        }
+    }
+}
